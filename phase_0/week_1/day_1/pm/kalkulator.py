bfb38f717e4b0c1a13fdb49c90967e97839a1cfe
Ivan Yapputra Yappi <vayxius@gmail.com>
2022-06-14 10:30:51 +0700

    Fix Folder

---
+++ phase_0/week_1/day_1/pm/kalkulator.py
@@ -0,0 +1,4 @@
+sample = "abc", "xyz"
+print(sample[1][0:2] + sample[0][-1] + ' ' + sample[0][0:2] + sample[1][-1])
+sample_string = 'dbca'
+print(sample_string[-1] + sample_string[1:3] + sample_string[0])
